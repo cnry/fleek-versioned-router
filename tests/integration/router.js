1d846de285c195dc3e3b8dc56e5487bf8fc6cd7d
Raymond Butcher <randomy@gmail.com>
2016-02-11 11:00:56 +0000

    Use red-tape and tap-summary

--- tests/integration/router.js
+++ tests/integration/router.js
@@ -1,43 +1,36 @@
 'use strict';
 
-const agent = require('supertest-koa-agent'),
-      app = require('../../example/app'),
+const client = require('..').client,
       expect = require('chai').expect,
-      test = require('tape');
+      test = require('red-tape');
 
-test('router', function(assert) {
-    assert.plan(3);
-    agent(app)
-        .get('/api/1.0.0/pet/1')
-        .end(function(err, res) {
-            expect(err).to.be.null;
-            expect(res.status).to.equal(200);
-            expect(res.body).to.deep.equal({
-                id: 1,
-                name: 'dog',
-                photoUrls: [],
-                version: '1.0.0'
-            });
-            assert.pass('GET /api/1.0.0/pet/1 should return a pet');
+test('router', function*(t) {
+
+    t.test('GET /api/1.0.0/pet/1 should return a pet', function*(t) {
+        const res = yield client.get('/api/1.0.0/pet/1').end();
+        expect(res.status).to.equal(200);
+        expect(res.body).to.deep.equal({
+            id: 1,
+            name: 'dog',
+            photoUrls: [],
+            version: '1.0.0'
         });
-    agent(app)
-        .get('/api/2.0.0/pet/1')
-        .end(function(err, res) {
-            expect(err).to.be.null;
-            expect(res.status).to.equal(200);
-            expect(res.body).to.deep.equal({
-                id: 1,
-                name: 'dog',
-                photoUrls: [],
-                version: '2.0.0'
-            });
-            assert.pass('GET /api/2.0.0/pet/1 should return a pet');
+    });
+
+    t.test('GET /api/2.0.0/pet/1 should return a pet', function*(t) {
+        const res = yield client.get('/api/2.0.0/pet/1').end();
+        expect(res.status).to.equal(200);
+        expect(res.body).to.deep.equal({
+            id: 1,
+            name: 'dog',
+            photoUrls: [],
+            version: '2.0.0'
         });
-    agent(app)
-        .get('/api/3.0.0/pet/1')
-        .end(function(err, res) {
-            expect(err).to.be.null;
-            expect(res.status).to.equal(404);
-            assert.pass('GET /api/3.0.0/pet/1 should return error 404');
-        });
+    });
+
+    t.test('GET /api/3.0.0/pet/1 should return error 404', function*(t) {
+        const res = yield client.get('/api/3.0.0/pet/1').end();
+        expect(res.status).to.equal(404);
+    });
+
 });
